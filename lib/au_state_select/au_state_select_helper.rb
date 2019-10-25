bcb6305cefda475a4b743c2641f85f9e3add3295
Amy Liu <amy.biyu.liu@gmail.com>
2019-10-25 17:41:57 +1030

    tweak

--- lib/au_state_select/au_state_select_helper.rb
+++ lib/au_state_select/au_state_select_helper.rb
@@ -1,14 +1,38 @@
-module AUStateSelect
-  module FormBuilder
-    def state_select(method, priority_state = nil, options = {}, html_options = {})
-      if Hash === priority_or_options
-        html_options = options
-        options = priority_or_options
-      else
-        options[:priority_state] = priority_or_options
-      end
+module ActionView
+  module Helpers
+    class FormBuilder
+      def state_select(method, priority_or_options = {}, options = {}, html_options = {})
+        if Hash === priority_or_options
+          html_options = options
+          options = priority_or_options
+        else
+          options[:priority_state] = priority_or_options
+        end
 
-      @template.country_select(@object_name, method, objectify_options(options), @default_options.merge(html_options))
+        @template.state_select(@object_name, method, objectify_options(options), @default_options.merge(html_options))
+      end
+    end
+
+    module FormOptionsHelper
+      def state_select(object, method, options = {}, html_options = {})
+        Tags::AUStateSelect.new(object, method, self, options, html_options).render
+      end
+    end
+    
+    module Tags
+      class AUStateSelect < Base
+        include ::AUStateSelect::TagHelper
+
+        def initialize(object_name, method_name, template_object, options, html_options)
+          @html_options = html_options
+
+          super(object_name, method_name, template_object, options)
+        end
+
+        def render
+          select_content_tag(state_option_tags, @options, @html_options)
+        end
+      end
     end
   end
 end
